8352025a781d6c1933cde827bf39505f486f3558
Willams Dias <willams88@gmail.com>
2025-04-16 12:27:36 -0300

    corrigido index funcional com eventos

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('arretados-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/compra.html',
+        '/confirmacao.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
