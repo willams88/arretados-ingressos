e6281fb0c46218778674d81852e33abb6a388a4a
willams88 <willamsdiassilva88@gmail.com>
2025-04-16 13:42:01 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
-<!-- Service worker para cache offline -->
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("arretados-cache").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/compra.html",
+        "/confirmacao.html"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
