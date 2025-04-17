cee4cf2780ff19fa42fa1931a9f13ea46ad7b77f
willams88 <willamsdiassilva88@gmail.com>
2025-04-17 02:50:52 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open("arretados-cache").then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/compra.html",
-        "/confirmacao.html"
-      ])
-    )
+    caches.open('arretados-cache').then(cache => {
+      return cache.addAll([
+        './index.html',
+        './compra.html',
+        './pagamento.html',
+        './confirmacao.html',
+        './historico.html',
+        './logo-arretados.jpg'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
